3dc7dced075df89ddf3223959504a10af9d6d6ba
Xotic750 <xotic750@gmail.com>
2017-07-14 17:33:16 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
 /**
- * @file
- * <a href="https://travis-ci.org/Xotic750/cross-console-x"
- * title="Travis status">
- * <img
- * src="https://travis-ci.org/Xotic750/cross-console-x.svg?branch=master"
- * alt="Travis status" height="18">
- * </a>
- * <a href="https://david-dm.org/Xotic750/cross-console-x"
- * title="Dependency status">
- * <img src="https://david-dm.org/Xotic750/cross-console-x.svg"
- * alt="Dependency status" height="18"/>
- * </a>
- * <a
- * href="https://david-dm.org/Xotic750/cross-console-x#info=devDependencies"
- * title="devDependency status">
- * <img src="https://david-dm.org/Xotic750/cross-console-x/dev-status.svg"
- * alt="devDependency status" height="18"/>
- * </a>
- * <a href="https://badge.fury.io/js/cross-console-x" title="npm version">
- * <img src="https://badge.fury.io/js/cross-console-x.svg"
- * alt="npm version" height="18">
- * </a>
- *
- * A cross-environment fix for missing methods.
- *
- * Requires ES3 or above.
- *
- * @version 1.5.0
+ * @file A cross-environment fix for missing methods.
+ * @version 1.6.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
