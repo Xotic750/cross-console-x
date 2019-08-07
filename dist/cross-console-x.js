c5571b76d22173eabe49b717297ff72193182b69
Graham Fairweather <xotic750@gmail.com>
2019-08-07 04:12:13 +0200

    Auto build

--- dist/cross-console-x.js
+++ dist/cross-console-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-06T21:59:02.834Z",
+  "date": "2019-08-07T02:12:01.577Z",
   "describe": "",
   "description": "A cross-environment fix for missing methods.",
   "file": "cross-console-x.js",
