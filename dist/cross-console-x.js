45b3519e35659ff518aa8a32a1934ff9d7930d3d
Graham Fairweather <xotic750@gmail.com>
2019-09-05 22:54:15 +0200

    Maintenance

--- dist/cross-console-x.js
+++ dist/cross-console-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-09-05T00:39:56.602Z",
+  "date": "2019-09-05T20:54:03.665Z",
   "describe": "",
   "description": "A cross-environment fix for missing methods.",
   "file": "cross-console-x.js",
